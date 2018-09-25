da6bc696ceaf51a2f1135308bc0707647bf155ac
leefaus <leefaus@github.com>
2018-09-25 17:25:23 -0400

    added post name

--- form-test.js
+++ form-test.js
@@ -1,6 +1,13 @@
 module.exports = async function(context) {
-    return {
-        status: 200,
-        body: "Hello, world!\n"
-    };
+  let body = 'something new...';
+  context.request.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+  });
+  context.request.on('end', () => {
+      console.log(body);
+  });
+  return {
+      status: 200,
+      body: context.request.body
+  };
 }
